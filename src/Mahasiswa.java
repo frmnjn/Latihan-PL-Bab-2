439d7249d4df350f27a05b5985f0d1c9bc3fe16d
Firman Budi Safrizal <firmanbudis@gmail.com>
2016-03-09 20:49:46 +0700

    tambah constructor dan method hitungJarak, hitungWaktuSatuPutaran ke kelas mahasiswa

--- src/Mahasiswa.java
+++ src/Mahasiswa.java
@@ -2,4 +2,17 @@ package praktikum_bab2;
 public class Mahasiswa{
     String nim, nama;
     int jum_putaran, jum_waktu;
+    
+    public Mahasiswa(String nim, String nama, int jum_putaran, int jum_waktu){
+        this.nim = nim;
+        this.nama = nama;
+        this.jum_putaran = jum_putaran;
+        this.jum_waktu = jum_waktu;
+    }
+    public int hitungJarak(Master_lapangan a){
+        return jum_putaran * a.hitungKeliling();
+    }
+    public double hitungWaktuSatuPutaran(){
+        return (double)jum_waktu / (double)jum_putaran;
+    }
 }
