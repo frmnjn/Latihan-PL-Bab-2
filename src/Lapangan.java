05f6dfff5c88090584821c508c07d30e217233a2
Firman Budi Safrizal <firmanbudis@gmail.com>
2016-03-09 20:45:24 +0700

    tambah method ke kelas lapangan

--- src/Lapangan.java
+++ src/Lapangan.java
@@ -1,5 +1,13 @@
 package praktikum_bab2;
 public class Lapangan{
-	int panjang_lap;
+    int panjang_lap;
     int lebar_lap;
+    
+    public Lapangan(int panjang_lap, int lebar_lap){
+    this.panjang_lap = panjang_lap;
+    this.lebar_lap = lebar_lap;
+    }
+    public int hitungKeliling(){
+        return 2 * (panjang_lap + lebar_lap);
+    }
 }
