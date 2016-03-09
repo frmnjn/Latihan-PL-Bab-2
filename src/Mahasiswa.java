ed4fdf7799eb146073c2e6e5523619dd1de4e451
Firman Budi Safrizal <firmanbudis@gmail.com>
2016-03-09 20:54:11 +0700

    ganti tipe data jadi double, tambah method cekTercepat di kelas mahasiswa dan file identifikasi

--- src/Mahasiswa.java
+++ src/Mahasiswa.java
@@ -1,7 +1,7 @@
 package praktikum_bab2;
 public class Mahasiswa{
     String nim, nama;
-    int jum_putaran, jum_waktu;
+    double jum_putaran, jum_waktu;
     
     public Mahasiswa(String nim, String nama, int jum_putaran, int jum_waktu){
         this.nim = nim;
@@ -9,10 +9,22 @@ public class Mahasiswa{
         this.jum_putaran = jum_putaran;
         this.jum_waktu = jum_waktu;
     }
-    public int hitungJarak(Master_lapangan a){
+    public double hitungJarak(Master_lapangan a){
         return jum_putaran * a.hitungKeliling();
     }
     public double hitungWaktuSatuPutaran(){
-        return (double)jum_waktu / (double)jum_putaran;
+        return jum_waktu / jum_putaran;
+    }
+    public void cekTercepat(Master_mahasiswa a[]){
+        double min;
+        int count = 0;
+        min = a[0].hitungWaktuSatuPutaran();
+        for(int i=0;i<a.length;i++){
+           if(min > a[i].hitungWaktuSatuPutaran()) {
+               min = a[i].hitungWaktuSatuPutaran();
+               count++;
+           }
+        }
+        System.out.println(a[count].nama);
     }
 }
